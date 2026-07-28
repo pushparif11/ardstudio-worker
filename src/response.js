6909694e05f40bcdd44712e78451ae895cd28546
pushparif11 <300842569+pushparif11@users.noreply.github.com>
2026-07-29 01:56:14 +0530

    Update response.js

--- src/response.js
+++ src/response.js
@@ -1,33 +1,27 @@
-import {HEADERS} from "./config.js";
+import { CORS_HEADERS } from "./config.js";
 
-export function success(imageBase64){
-
-    return new Response(JSON.stringify({
-
-        success:true,
-        imageBase64:imageBase64
-
-    }),{
-
-        status:200,
-        headers:HEADERS
-
-    });
-
+export function success(data, status = 200) {
+  return new Response(
+    JSON.stringify({
+      success: true,
+      ...data
+    }),
+    {
+      status,
+      headers: CORS_HEADERS
+    }
+  );
 }
 
-export function error(message){
-
-    return new Response(JSON.stringify({
-
-        success:false,
-        error:message
-
-    }),{
-
-        status:400,
-        headers:HEADERS
-
-    });
-
+export function error(message, status = 400) {
+  return new Response(
+    JSON.stringify({
+      success: false,
+      error: message
+    }),
+    {
+      status,
+      headers: CORS_HEADERS
+    }
+  );
 }

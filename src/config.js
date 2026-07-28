a128ef91a0bde08384870ab8836d4023d794b014
pushparif11 <300842569+pushparif11@users.noreply.github.com>
2026-07-29 01:42:48 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,9 @@
+export const OPENAI_URL =
+"https://api.openai.com/v1/images/edits";
 
+export const HEADERS = {
+    "Access-Control-Allow-Origin":"*",
+    "Access-Control-Allow-Headers":"Content-Type, Authorization",
+    "Access-Control-Allow-Methods":"GET,POST,OPTIONS",
+    "Content-Type":"application/json"
+};

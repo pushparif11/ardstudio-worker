abfcbfdc6cda91d650da708c88c5691cc9031abe
pushparif11 <300842569+pushparif11@users.noreply.github.com>
2026-07-29 01:43:24 +0530

    Update response.js

--- src/response.js
+++ src/response.js
@@ -1,1 +1,33 @@
+import {HEADERS} from "./config.js";
 
+export function success(imageBase64){
+
+    return new Response(JSON.stringify({
+
+        success:true,
+        imageBase64:imageBase64
+
+    }),{
+
+        status:200,
+        headers:HEADERS
+
+    });
+
+}
+
+export function error(message){
+
+    return new Response(JSON.stringify({
+
+        success:false,
+        error:message
+
+    }),{
+
+        status:400,
+        headers:HEADERS
+
+    });
+
+}

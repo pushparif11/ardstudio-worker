e736ab1594040166313e82d922424b03b1b04c1d
pushparif11 <300842569+pushparif11@users.noreply.github.com>
2026-08-01 13:05:54 +0530

    Update huggingface.js

--- src/providers/huggingface.js
+++ src/providers/huggingface.js
@@ -1,6 +1,5 @@
 const API_URL =
-  "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-inpainting";
-
+  "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-2-inpainting";
 export async function editImage(env, imageBase64, prompt) {
   const response = await fetch(API_URL, {
     method: "POST",
